7294da5bb1cb45c97150dca0e7dddd0cabc2eeac
har-neptune <muhtria.har@gmail.com>
2025-12-17 16:58:48 +0700

    Finalize MLProject structure and advanced CI workflow

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,55 +1,47 @@
 import numpy as np
 import mlflow
-import dagshub
-
+import mlflow.sklearn
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score
 
 
 def main():
-    # Init DagsHub MLflow
-    dagshub.init(
-        repo_owner="har-neptune",
-        repo_name="world-happiness-mlflow",
-        mlflow=True
-    )
-
     # Load preprocessed data
-    X_train = np.load("namadataset_preprocessing/X_train.npy")
-    X_test = np.load("namadataset_preprocessing/X_test.npy")
-    y_train = np.load("namadataset_preprocessing/y_train.npy")
-    y_test = np.load("namadataset_preprocessing/y_test.npy")
+    X_train = np.load("worldhappiness_preprocessing/X_train.npy")
+    X_test = np.load("worldhappiness_preprocessing/X_test.npy")
+    y_train = np.load("worldhappiness_preprocessing/y_train.npy")
+    y_test = np.load("worldhappiness_preprocessing/y_test.npy")
 
-    # Model
-    model = RandomForestRegressor(
-        n_estimators=100,
-        random_state=42
-    )
+    # MLflow setup
+    mlflow.set_tracking_uri("http://127.0.0.1:5000")
+    mlflow.set_experiment("World Happiness Baseline Model")
 
     with mlflow.start_run(run_name="rf_baseline"):
-        # Train
+        # Train model (baseline)
+        model = RandomForestRegressor(
+            n_estimators=100,
+            random_state=42
+        )
         model.fit(X_train, y_train)
 
-        # Predict
+        # Prediction & metrics
         y_pred = model.predict(X_test)
 
-        # Metrics
-        mse = mean_squared_error(y_test, y_pred)
-        rmse = mse ** 0.5
-        mae = mean_absolute_error(y_test, y_pred)
+        rmse = mean_squared_error(y_test, y_pred) ** 0.5
         r2 = r2_score(y_test, y_pred)
-
 
         # Manual logging
         mlflow.log_param("model_type", "RandomForestRegressor")
         mlflow.log_param("n_estimators", 100)
 
         mlflow.log_metric("rmse", rmse)
-        mlflow.log_metric("mae", mae)
         mlflow.log_metric("r2", r2)
 
         # Log model
-        mlflow.sklearn.log_model(model, artifact_path="model")
+        mlflow.sklearn.log_model(
+            model,
+            artifact_path="model"
+        )
 
 
 if __name__ == "__main__":
